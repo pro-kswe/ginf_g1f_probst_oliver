00eeef8cda708edd23ae03743b11f0c53585bf58
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2025-09-04 15:21:13 +0200

    Dreieck und Rechteck mit Variablen

---
+++ 04_variablen/02_dreieck.py
@@ -0,0 +1,8 @@
+import turtle
+
+a = 250
+turtle.fd(a)
+turtle.lt(120)
+turtle.fd(a)
+turtle.lt(120)
+turtle.fd(a)
---
+++ 04_variablen/03_rechteck.py
@@ -0,0 +1,12 @@
+import turtle
+
+a = 200
+b = 100
+turtle.fd(a)
+turtle.lt(90)
+turtle.fd(b)
+turtle.lt(90)
+turtle.fd(a)
+turtle.lt(90)
+turtle.fd(b)
+turtle.lt(90)
