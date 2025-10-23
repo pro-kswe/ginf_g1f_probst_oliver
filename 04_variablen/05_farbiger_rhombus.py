a173b16a4d5c0d4644b53df1a06f1e97810e2de7
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2025-10-23 09:13:16 +0200

    Farbiger Rhombus

---
+++ 04_variablen/05_farbiger_rhombus.py
@@ -0,0 +1,18 @@
+import turtle
+
+a = 100
+alpha = 120
+beta = 60
+farbe = "blue"
+staerke = 3
+turtle.pencolor(farbe)
+turtle.pensize(staerke)
+turtle.lt(30)
+turtle.fd(a)
+turtle.lt(alpha)
+turtle.fd(a)
+turtle.lt(beta)
+turtle.fd(a)
+turtle.lt(alpha)
+turtle.fd(a)
+turtle.lt(beta)
