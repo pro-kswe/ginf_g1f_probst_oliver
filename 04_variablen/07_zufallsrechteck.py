5089be3843015f4f2e87f56cac18805f3c2dcf4b
Oliver Probst <pro@kswe.ch>
2025-11-02 10:54:25 +0100

    Zufallsrechteck

---
+++ 04_variablen/07_zufallsrechteck.py
@@ -0,0 +1,13 @@
+import turtle
+import random
+
+a = random.randrange(50, 101)
+b = random.randrange(25, 41)
+turtle.forward(a)
+turtle.left(90)
+turtle.forward(b)
+turtle.left(90)
+turtle.forward(a)
+turtle.left(90)
+turtle.forward(b)
+turtle.left(90)
