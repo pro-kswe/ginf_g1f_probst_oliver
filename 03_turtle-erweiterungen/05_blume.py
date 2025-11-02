7d7cfab8baabae7dfb5f3ec1019f55ada58b4339
Oliver Probst <pro@kswe.ch>
2025-11-02 10:53:54 +0100

    Blume

---
+++ 03_turtle-erweiterungen/05_blume.py
@@ -0,0 +1,40 @@
+import turtle
+
+turtle.speed(10)
+turtle.pu()
+
+turtle.goto(20, 20)
+turtle.pencolor("yellow")
+turtle.dot(80)
+
+turtle.goto(20, 80)
+turtle.pencolor("red")
+turtle.dot(40)
+
+turtle.goto(20, -40)
+turtle.pencolor("purple")
+turtle.dot(40)
+
+turtle.goto(60, 60)
+turtle.pencolor("orange")
+turtle.dot(40)
+
+turtle.goto(-20, -20)
+turtle.pencolor("blue")
+turtle.dot(40)
+
+turtle.goto(60, -20)
+turtle.pencolor("pink")
+turtle.dot(40)
+
+turtle.goto(-20, 60)
+turtle.pencolor("green")
+turtle.dot(40)
+
+turtle.goto(80, 20)
+turtle.pencolor("cyan")
+turtle.dot(40)
+
+turtle.goto(-40, 20)
+turtle.pencolor("magenta")
+turtle.dot(40)
