2f2ba59409c68875bc88cbc67567f1655faae276
Oliver Probst <pro@kswe.ch>
2025-11-02 10:54:48 +0100

    Kreise

---
+++ 04_variablen/08_kreise.py
@@ -0,0 +1,25 @@
+import turtle
+import random
+
+radius = random.randrange(50, 76)
+farbe_1 = "red"
+farbe_2 = "green"
+farbe_3 = "blue"
+farbe_4 = "pink"
+turtle.pencolor(farbe_1)
+turtle.circle(radius)
+turtle.pu()
+turtle.fd(radius)
+turtle.pd()
+turtle.pencolor(farbe_2)
+turtle.circle(radius)
+turtle.pu()
+turtle.fd(radius)
+turtle.pd()
+turtle.pencolor(farbe_3)
+turtle.circle(radius)
+turtle.pu()
+turtle.fd(radius)
+turtle.pd()
+turtle.pencolor(farbe_4)
+turtle.circle(radius)
