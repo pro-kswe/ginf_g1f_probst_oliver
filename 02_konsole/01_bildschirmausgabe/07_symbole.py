31d44633523804a96c39bc48e1208843cde592a4
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2025-12-12 17:34:28 +0100

    Symbole

--- 02_konsole/01_bildschirmausgabe/07_symbole.py
+++ 02_konsole/01_bildschirmausgabe/07_symbole.py
@@ -1,6 +1,14 @@
 import random as rd
 
-symbole = ["*", "$", "€", ":-)", ":-("]
+print("Variante A")
+symbole = ["*", "$", "€"]
 symbol = rd.choice(symbole)
 for _ in range(4):
   print(4 * symbol)
+
+print("Variante B")
+symbole = ["****", "$$$$", "€€€€"]
+symbol = rd.choice(symbole)
+
+for _ in range(4):
+    print(symbol)
