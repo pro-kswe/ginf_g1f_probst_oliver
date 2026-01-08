f7093a08ef0d907040efb615b23e5f88e68b77f7
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2026-01-08 15:22:22 +0100

    Beispiel

---
+++ 02_konsole/03_arithmetik/01_beispiel_1.py
@@ -0,0 +1,8 @@
+import random as rd
+
+a = rd.randrange(10, 101)
+umfang = a + a + a + a
+flaecheninhalt = a * a
+print(f"Seitenlänge: {a}")
+print(f"Umfang: {umfang}")
+print(f"Flächeninhalt: {flaecheninhalt}")
