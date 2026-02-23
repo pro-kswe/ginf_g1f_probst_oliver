a0a35a4bbd2e11404d0604bdd2ca8c48a6cac2a4
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2026-02-23 14:28:50 +0100

    Temperatur

---
+++ 02_konsole/03_arithmetik/05_temperatur.py
@@ -0,0 +1,5 @@
+temperatur_in_celsius = 15.5
+temperatur_in_fahrenheit = (temperatur_in_celsius * 9 / 5) + 32
+print("Temperaturumrechnung")
+print(f"Temperatur in Celsius: {temperatur_in_celsius}")
+print(f"Temperatur in Fahrenheit: {temperatur_in_fahrenheit}")
