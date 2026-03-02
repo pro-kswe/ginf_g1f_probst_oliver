69d4dc9304c07aef477f4bcc9032b5fdff551ed3
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2026-03-02 16:33:08 +0100

    Start Teamchef

---
+++ 02_konsole/04_benutzereingabe/06_teamchef.py
@@ -0,0 +1,11 @@
+# Übung 26
+# Tipp: vier Eingaben programmieren
+# Tipp: einmal choice für die Namen verwenden
+# Achtung: In den eckigen Klammern müssen die vier Variablen der Eingabe stehen!
+import random as rd
+
+print(???)
+name_ 1 = ???
+namen = [???, ???, ???, ???]
+chef = rd.choice(namen)
+print(???)
