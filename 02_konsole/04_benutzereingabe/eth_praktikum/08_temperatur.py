f48430446582609129f45f611e7511b0fda1522a
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2026-03-02 17:21:05 +0100

    Temperatur mit Input

---
+++ 02_konsole/04_benutzereingabe/eth_praktikum/08_temperatur.py
@@ -0,0 +1,4 @@
+print("Temperaturumrechnung")
+temperatur_in_celsius = float(input("Temperatur in Celsius?"))
+temperatur_in_fahrenheit = (temperatur_in_celsius * 9 / 5) + 32
+print(f"Temperatur in Fahrenheit: {temperatur_in_fahrenheit}")
