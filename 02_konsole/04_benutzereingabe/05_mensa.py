aa940f2deaa905c3acea056e59ab891dae868c22
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2026-03-02 16:30:46 +0100

    Beispiel und Startt Übung 25

---
+++ 02_konsole/04_benutzereingabe/05_mensa.py
@@ -0,0 +1,8 @@
+# Übung 25
+# Tipp: zwei Eingaben programmieren
+# Tipp: zwei Mal choice für die Namen und die Bewertung 
+#       verwenden
+import random as rd
+
+namen = ["Alice", "Bob", "Carol", "Eve"]
+name = rd.choice(namen)
