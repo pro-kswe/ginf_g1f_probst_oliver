4efbef4ce6f30e87f7215c2f423fcc6ce0bf04bc
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2026-03-23 14:22:18 +0100

    Lösungen

---
+++ 02_konsole/04_benutzereingabe/07_zeichen_zaehlen.py
@@ -0,0 +1,3 @@
+passwort = input("Bitte geben Sie Ihr Passwort ein:")
+anzahl_zeichen = len(passwort)
+print(f"Ihr Passwort ist {anzahl_zeichen} Zeichen lang.")
---
+++ 02_konsole/04_benutzereingabe/08_zeichen_zaehlen_2.py
@@ -0,0 +1,5 @@
+for _ in range(3):
+    name = input(f"Bitte geben Sie einen Namen ein:")
+    anzahl_zeichen = len(name)
+    print(f"Der Name {name} besitzt {anzahl_zeichen} Zeichen.")
+    print("================")
