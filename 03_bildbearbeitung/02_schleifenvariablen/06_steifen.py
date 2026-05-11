cefd32d1caba73ab07113e423f32600f7155c0ef
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2026-05-11 16:19:38 +0200

    Streifen

---
+++ 03_bildbearbeitung/02_schleifenvariablen/06_steifen.py
@@ -0,0 +1,14 @@
+import PIL.Image as img
+import random as rd
+
+bild = img.new("RGB", (20, 3))
+
+for x in range(0, 20):
+    r = rd.randrange(0, 256)
+    g = rd.randrange(0, 256)
+    b = rd.randrange(0, 256)
+    bild.putpixel((x, 0), (r, g, b))
+    bild.putpixel((x, 1), (r, g, b))
+    bild.putpixel((x, 2), (r, g, b))
+
+bild.save("06_streifen_ergebnis.png") 
