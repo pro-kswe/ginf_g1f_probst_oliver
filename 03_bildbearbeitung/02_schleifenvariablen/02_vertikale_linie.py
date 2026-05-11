25b93e0f40f92a01ea7a1c5e214fc8320f449d58
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2026-05-11 15:48:25 +0200

    Lösung Vertikale Linie

--- 03_bildbearbeitung/02_schleifenvariablen/02_vertikale_linie.py
+++ 03_bildbearbeitung/02_schleifenvariablen/02_vertikale_linie.py
@@ -6,8 +6,14 @@ bild = img.new("RGB", (5, 10))
 r = rd.randrange(0, 256)
 g = rd.randrange(0, 256)
 b = rd.randrange(0, 256)
+farbe = (r, g, b)
 
+# Variante 1
 for y in range(0, 10):
-    bild.putpixel((2, y), (r, g, b))
+    bild.putpixel((2, y), farbe)
+
+# Variante 2
+for y in range(0, 10):
+    bild.putpixel((2, y), (r, b, g))
 
 bild.save("02_vertikale_linie_ergebnis.png") 
