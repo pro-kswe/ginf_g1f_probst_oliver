783183dba7bee86c4c24a7177cc1aa293f715f76
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2026-06-02 07:48:46 +0200

    Farbe

---
+++ 04_if-anweisung/03_farbe.py
@@ -0,0 +1,7 @@
+farbe = input("Bitte geben Sie Ihre Lieblingsfarbe ein:")
+print(f"Ihre Lieblingsfarbe lautet: {farbe}")
+
+if farbe == "gelb":
+    print("Eine schöne Farbe.")
+
+print("Vielen Dank.")
